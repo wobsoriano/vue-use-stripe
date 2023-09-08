2dd2c3f99adda3bc2fcccf2df99a2b2678dc8ffa
wobsoriano <sorianorobertc@gmail.com>
2023-09-07 21:58:59 -0700

    update playground tsconfig

--- dev/vite.config.ts
+++ dev/vite.config.ts
@@ -5,9 +5,4 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      'my-library': path.resolve(__dirname, '../src'),
-    },
-  },
 })
